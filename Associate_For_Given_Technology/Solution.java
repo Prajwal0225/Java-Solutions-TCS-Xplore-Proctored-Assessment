0124fc7afc5f1d792c3fab3c16f48a8827386429
Prajwal <103810119+Prajwal0225@users.noreply.github.com>
2024-10-03 09:38:54 +0530

    Update Solution.java

--- Associate_For_Given_Technology/Solution.java
+++ Associate_For_Given_Technology/Solution.java
@@ -1,64 +1,82 @@
-import java.util.Arrays;
-import java.util.Scanner;
-public class Solution {
-    public static void main(String[] args) {
-        Associate[] associates = new Associate[5];
-        Scanner sc = new Scanner(System.in);
-        for(int i = 0;i < 5;i++) {
-            associates[i] = new Associate(sc.nextInt(), sc.next(), sc.next(), sc.nextInt());
-        }
-        String searchTechnology = sc.next();
-        sc.close();
-        Associate[] result= associatesForGivenTechnology(associates,searchTechnology);
-        for(int i=0;i<result.length;i++)
-            System.out.println(result[i].getId());
-    }
+import java.util.*;
 
-    public static Associate[] associatesForGivenTechnology(Associate[] associates, String searchTechnology) {
-        Associate[] refined = new Associate[0];
-        for(int i=0;i<associates.length;i++) {
-            if(associates[i].getTechnology().equals(searchTechnology) && associates[i].getExperienceInYears()%5==0){
-                refined=Arrays.copyOf(refined,refined.length+1);
-                refined[refined.length-1] = associates[i];
+public class Solution{
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		Associate arr[] = new Associate[5];
+		for(int i = 0;i<5;i++){
+			arr[i] = new Associate(sc.nextInt(),sc.next(),sc.next(),sc.nextInt());
+		}	
+        String searchTechnology = sc.next();
+        Associate ans[] = associatesForGivenTechnology(arr,searchTechnology);
+        for(int i = 0;i<ans.length;i++){
+            System.out.println(ans[i].getid());
+        }
+   }
+		
+				
+    public static Associate[] associatesForGivenTechnology(Associate arr[],String tech){
+        int count = 0;
+        for(int i = 0;i<5;i++){
+            if(tech.equals(arr[i].gettechnology()) && arr[i].getexperienceInYears() % 5 == 0){
+                count++;
             }
         }
-        return refined;
+        Associate arr1[] = new Associate[count];
+        int k = 0;
+        for(int i = 0;i<5;i++){
+            if(tech.equals(arr[i].gettechnology()) && arr[i].getexperienceInYears() % 5 == 0){
+            arr1[k] = arr[i];
+            k++;
+            }
+        }
+        return arr1;
     }
 }
 
 class Associate{
-    private int id;
-    private String name;
-    private String technology;
-    private int experienceInYears;
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getTechnology() {
-        return technology;
-    }
-    public void setTech(String technology) {
-        this.technology = technology;
-    }
-    public int getExperienceInYears() {
-        return experienceInYears;
-    }
-    public void setExp(int experienceInYears) {
-        this.experienceInYears = experienceInYears;
-    }
-    public Associate(int id, String name, String technology, int experienceInYears) {
-        this.id = id;
-        this.name = name;
-        this.technology = technology;
-        this.experienceInYears = experienceInYears;
-    }
+	private int id;
+	private String name;
+	private String technology;
+	private int experienceInYears;
+
+
+Associate(int id,String name,String technology,int experienceInYears){
+		this.id = id;
+		this.name = name;
+		this.technology = technology;
+		this.experienceInYears = experienceInYears;
+	}
+	
+	void setid(int id){
+		this.id = id;
+	}
+	int getid(){
+		return this.id;
+	}
+	void setname(String name){
+		this.name = name;
+	}
+
+	String getname(){
+		return this.name;
+	}
+
+	void settechnology(String technology){
+		this.technology = technology;
+	}
+
+	String gettechnology(){
+		return this.technology;
+	}
+
+	void setexperienceInYears(int experienceInYears){
+		this.experienceInYears = experienceInYears;
+	}
+
+	int getexperienceInYears(){
+		return this.experienceInYears;
+	}
+
+	
 }
